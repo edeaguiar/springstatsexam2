77a4d162bb01ab86b249d60584c943eb2b8da19a
edeaguiar <63740806+edeaguiar@users.noreply.github.com>
2020-04-16 22:15:40 +0200

    Rename functions
    Rename all functions so that we know what they do!

--- R/ConfidenceIntervals.R
+++ R/ConfidenceIntervals.R
@@ -1,1 +1,101 @@
 # Confidence Intervals functions
+#' Error margin for normal distribution CI with a population standard deviation where n is large
+#'
+#' @param CI Confidence interval expressed as a decimal
+#' @param n Sample size
+#' @param population.sd Population standard deviation
+#'
+#' @return
+#' @export
+#'
+#' @examples
+error.margin <- function(CI, n, sd, large = TRUE)
+{
+  if (large)
+  {
+    list(method = "proceed with upper and lower bound as normal",
+         errormargin = abs(qnorm((CI + (
+           1 - CI
+         ) / 2), lower.tail = F) * sd / sqrt(n)))
+  }
+  else
+  {
+    list(
+      method = "small.sample.size",
+      errormargin.studentTdistribution = (qt((1 - CI) / 2, n - 1, lower.tail = F)) *
+        sd / sqrt(n)
+    )
+  }
+}
+#' Margin of error for difference between population means
+#'
+#' @param CI Confidence interval expressed as a decimal
+#' @param x1 Vector (bigger values)
+#' @param x2 Vector (smaller values)
+#'
+#' @return
+#' @export
+#'
+#' @examples
+CI.diff <- function(CI, x1, x2)
+{
+  (qt((1 - CI) / 2, length(x1) - 1, lower.tail = F)) * sd(x1 - x2) / sqrt(length(x1))
+}
+#' Margin of Error for difference between paired population means
+#'
+#' @param CI Confidence interval expressed as a decimal
+#' @param ssd1 Sample Standard Deviation 1
+#' @param ssd2 Sample Standard Deviation 2
+#' @param n1 Sample size 1
+#' @param n2 Sample size 2
+#'
+#' @return
+#' @export
+#'
+#' @examples
+CI.paired.diff<-function(CI,ssd1,ssd2,n1,n2,large=TRUE)
+{
+  if (large)
+  {
+    large = qnorm(CI + (1 - CI) / 2) * sqrt(ssd1 ^ 2 / n1 + ssd2 ^ 2 / n2)
+  }
+  else
+  {
+    small = abs(qt(abs((CI + (
+      1 - CI
+    ) / 2)), (ssd1 ^ 2 / n1 + ssd2 ^ 2 / n2) ^ 2 / ((ssd1 ^ 2 / n1) ^ 2 / (n1 -
+                                                                             1) + (ssd2 ^ 2 / n2) ^ 2 / (n2 - 1)
+    ), low = F))
+  }
+}
+#' Margin of error for a single large population proportion
+#'
+#' @param CI Confidence interval expressed as a decimal
+#' @param p1 Number of subjects 1 (numerator of proportion)
+#' @param n Sample size
+#'
+#' @return
+#' @export
+#'
+#' @examples To calculate bounds, subtract/add this functions output to p=(p1+2)/(n+4)
+CI.prop<-function(CI,p1,n)
+{
+  qnorm(CI+(1-CI)/2)*sqrt((p1+2)/(n+4)*(1-(p1+2)/(n+4))/(n+4))
+}
+#' Margin of error for the difference between two population (paired) proportions, large
+#'
+#' @param CI Confidence interval, expressed as a decimal
+#' @param S1 Number of subjects 1 (numerator of proportion)
+#' @param S2 Number of subjects 2 (numerator of proportion)
+#' @param n1 Sample size 1
+#' @param n2 Sample size 2
+#'
+#' @return
+#' @export
+#'
+#' @examples To compute p1 and p2 from the S and n for calculating the lower and upper bounds: p1=(s1+1)/(n1+2) and p2=(s2+1)/(n2+2). Subtract p1 (the bigger proportion) from the smaller p2 and then subtract/add the margin of error
+CI.paired.prop.diff<-function(CI,S1,S2,n1,n2)
+{
+  qnorm(CI+(1-CI)/2)*sqrt((S1+1)/(n1+2)*(1-(S1+1)/(n1+2))/(n1+2)+(S2+1)/(n2+2)*(1-(S2+1)/(n2+2))/(n2+2))
+}
+
--- R/HypothesisTesting.R
+++ R/HypothesisTesting.R
@@ -1,16 +1,185 @@
 #NewHypothesisTesting
-#' Calculates basic test statistic, variance is known, n is large
+#' Calculates basic test statistic Z or T
 #'
 #' @param xbar Sample mean
 #' @param mu Hypothesized sample mean
 #' @param sd Standard deviation
 #' @param n Sample size
+#' @param large Treat the sample as large?
+#' @param comparison notequal, greaterthan or lessthan
 #'
 #' @return The test statistic
 #' @export
 #'
 #' @examples
-Z <- function(xbar, mu, sd, n)
+test.stat <- function(xbar, mu, sd, n, large = TRUE, comparison="notequal")
 {
-  (xbar-mu)/(sd/sqrt(n))
+  RFunc = ""
+  if (comparison == "notequal" |
+      comparison == "ne" |
+      comparison == "<>" | comparison == "!=") {
+    RFunc = "2x + low F"
+  }
+  else if (comparison == "lessthan" |
+           comparison == "lt" | comparison == "<") {
+    RFunc = NA
+  }
+  else if (comparison == "greaterthan" |
+           comparison == "gt" | comparison == ">") {
+    RFunc = "low F"
+  }
+  else {
+    stop("Unknown comparison")
+  }
+
+  if (large)
+  {
+    list(method = "pnorm",
+         R.Func = RFunc,
+         test.stat.Z = (xbar - mu) / (sd / sqrt(n)))
+  }
+  else
+  {
+    list(
+      method = "pt",
+      R.Func = RFunc,
+      test.stat.T = (xbar - mu) / (sd / sqrt(n)),
+      degrees.free = (n - 1))
+  }
 }
+
+another_function<-function()
+{
+
+}
+
+#' Hypothesis test for a VECTOR that is population mean difference. Outputs values needed to put into pt
+#'
+#' @param x Vector
+#' @param mu Hypothesized sample mean
+#'
+#' @return
+#' @export
+#'
+#' @examples
+H.diff.small.nopopvariance<-function(x,mu)
+{
+  list((mean(x)-mu)/(sd(x)/sqrt(length(x))),length(x)-1)
+}
+
+#' Calculates Z test stat for a difference between two (paired) population proportions, population variances unknown and not assumed equal. Gives inputs for pnorm
+#'
+#' @param xbar1 Average for bigger data
+#' @param xbar2 Average for smaller data
+#' @param ssd1 Sample Standard Deviation of the bigger data
+#' @param ssd2 Sample Standard Deviation of the smaller data
+#' @param n1 Bigger sample size
+#' @param n2 Smaller sample size
+#'
+#' @return
+#' @export
+#'
+#' @examples
+H.diff.prop.nopopvariance<-function(xbar1,xbar2,ssd1,ssd2,n1,n2,islarge=TRUE)
+{
+  if (islarge)
+  {
+    list(method="pnorm",
+         Z=(xbar1-xbar2)/sqrt((ssd1^2/n1)+(ssd2^2/n2)))
+  }
+  else
+  {
+    list(method="pt",
+         T=(xbar1-xbar2)/sqrt(ssd1/4+ssd2/6),
+         Student.T.Dist=(ssd1/4+ssd2/6)^2/((ssd1/4)^2/(4-1)+(ssd2/6)^2/(6-1)))
+  }
+}
+
+#' Calculates Z test statistic for a population proportion, variance is unknown, N is large
+#'
+#' @param p1 The numerator of the actual proportion
+#' @param n The sample size (denominator of the actual proportion)
+#' @param p0 The wanted proportion as a decimal
+#'
+#' @return
+#' @export
+#'
+#' @examples
+H.prop.l.nv<-function(p1,n,p0)
+{
+  ((p1/n)-p0)/sqrt(p0*(1-p0)/n)
+}
+
+#' Calculates Z test statistic for a difference between two (paired) popultation proportions
+#'
+#' @param p1 How many out of the sample size for first data
+#' @param s1 Sample size for first data
+#' @param p2 How many out of the sample size for first data
+#' @param s2 Sample size for second data
+#'
+#' @return
+#' @export
+#'
+#' @examples
+H.paired.prop.l.nv<-function(p1,s1,p2,s2)
+{
+  ((p2/s2)-(p1/s1))/(sqrt(((p2+p1)/(s2+s1))*(1-((p2+p1)/(s2+s1)))*(1/s1+1/s2)))
+}
+
+#' Calculates t test for paired small vectors
+#'
+#' @param x1 First Vector
+#' @param x2 Second Vector
+#'
+#' @return
+#' @export
+#'
+#' @examples
+H.Vectorsgiven.paired.small.nopopvariance<-function(x1,x2)
+{
+  t.test(x1,x2,paired = T)
+}
+
+#' Standard deviation p hat for significance level
+#'
+#' @param p proportion to be tested, a decimal
+#' @param n Sample size
+#'
+#' @return
+#' @export
+#'
+#' @examples
+z.ignore.calculates.p.hat.sd.for.detsiglevel<-function(p,n)
+{
+  sqrt(p*(1-p)/n)
+}
+
+#' Calculates the value of p hat for calculating significance level
+#'
+#' @param H0 null hypothesis threshold
+#' @param n sample size
+#'
+#' @return
+#' @export
+#'
+#' @examples
+z.ignore.calculates.p.hat.for.detsiglevel<-function(H0,n)
+{
+  H0/n
+}
+
+#' Determines the significance level
+#'
+#' @param p proportion as a decimal
+#' @param H0 null hypothesis threshold
+#' @param n Sample size
+#'
+#' @return
+#' @export
+#'
+#' @examples
+detsiglevel<-function(p,H0,n)
+{
+  pnorm(z.ignore.calculates.p.hat.for.detsiglevel(H0,n),p,z.ignore.calculates.p.hat.sd.for.detsiglevel(p,n))
+}
+
